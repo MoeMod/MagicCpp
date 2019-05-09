c3c9a4bb6a08000bfa9e94758cb9c43b12ffd324
MoeMod <824395314@qq.com>
2019-05-09 18:03:19 +0800

    tuple cart forwarding

--- TupleCartesian元祖的笛卡尔积.cpp
+++ TupleCartesian元祖的笛卡尔积.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <tuple>
 
-template<class T, class Tuple2, std::size_t...I>
-constexpr auto cart_ext_impl(std::tuple<T> a, Tuple2 rhs, std::index_sequence<I...>)
+template<class Tuple1, class Tuple2>
+constexpr auto cart_impl(Tuple1&& lhs, Tuple2&& rhs, std::index_sequence<0>, 
+	typename std::enable_if<(std::tuple_size<typename std::decay<Tuple1>::type>::value == std::tuple_size<typename std::decay<Tuple2>::type>::value == 1)>::type * = nullptr)
 {
-	return std::tuple<std::pair<T, typename std::tuple_element<I, Tuple2>::type>...>{
-		{std::get<0>(a), std::get<I>(rhs)}...
-	};
+	using T1 = typename std::tuple_element<0, typename std::decay<Tuple1>::type >::type;
+	using T2 = typename std::tuple_element<0, typename std::decay<Tuple2>::type >::type;
+	return std::tuple<std::pair<T1, T2>>(std::pair<T1, T2>(std::get<0>(std::forward<Tuple1>(lhs)), std::get<0>(std::forward<Tuple2>(rhs))));
 }
 
-template<class T, class Tuple2>
-constexpr auto cart_ext(std::tuple<T> a, Tuple2 rhs)
+template<class T, class Tuple2, std::size_t...I2>
+constexpr auto cart_impl(const std::tuple<T>& lhs, Tuple2&& rhs, std::index_sequence<I2...>, 
+	typename std::enable_if<std::tuple_size<typename std::decay<Tuple2>::type>::value != 1>::type * = nullptr)
 {
-	return cart_ext_impl(a, rhs, std::make_index_sequence<std::tuple_size<Tuple2>::value>());
+	return std::tuple_cat(cart_impl(
+		lhs, 
+		std::tuple<typename std::tuple_element<I2, typename std::decay<Tuple2>::type>::type>(std::get<I2>(std::forward<Tuple2>(rhs))), 
+		std::index_sequence<0>()
+	)...);
 }
 
-template<class Tuple1, class Tuple2, std::size_t...I>
-constexpr auto cart_impl(Tuple1 lhs, Tuple2 rhs, std::index_sequence<I...>)
+template<class Tuple1, class Tuple2, std::size_t...I1>
+constexpr auto cart_impl(Tuple1&& lhs, Tuple2&& rhs, std::index_sequence<I1...>, 
+	typename std::enable_if<std::tuple_size<typename std::decay<Tuple1>::type>::value != 1>::type * = nullptr)
 {
-	return std::tuple_cat(cart_ext(std::tuple<typename std::tuple_element<I, Tuple1>::type>(std::get<I>(lhs)), rhs)...);
+	return std::tuple_cat(cart_impl(
+		std::tuple<typename std::tuple_element<I1, typename std::decay<Tuple1>::type>::type>(std::get<I1>(std::forward<Tuple1>(lhs))), 
+		rhs, 
+		std::make_index_sequence<std::tuple_size< typename std::decay<Tuple2>::type >::value>()
+	)...);
 }
 
 template<class Tuple1, class Tuple2>
-constexpr auto cart(Tuple1 lhs, Tuple2 rhs)
+constexpr auto cart(Tuple1&& lhs, Tuple2&& rhs)
 {
-	return cart_impl(lhs, rhs, std::make_index_sequence<std::tuple_size<Tuple1>::value>());
+	return cart_impl(std::forward<Tuple1>(lhs), std::forward<Tuple2>(rhs), std::make_index_sequence<std::tuple_size< typename std::decay<Tuple1>::type >::value>());
 }
 
 int main()
