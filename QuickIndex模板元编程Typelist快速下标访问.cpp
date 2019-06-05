01d7f7507f906697073d3d0750e8bd618f115d9a
moemod <824395314@qq.com>
2019-06-05 19:23:14 +0800

    quicker typelist index

--- QuickIndex模板元编程Typelist快速下标访问.cpp
+++ QuickIndex模板元编程Typelist快速下标访问.cpp
@@ -1,42 +1,67 @@
 #include <iostream>
 #include <type_traits>
 
-// Modern one
-template<size_t I, class T> struct Indexed { using type = T; };
-template<class...Ts> struct MakeIndexFinder
-{
-	template<class Seq> struct impl;
-	template<size_t...I> struct impl<std::index_sequence<I...>> : Indexed<I, Ts>... {};
-	using type = impl<std::index_sequence_for<Ts...>>;
-};
-template<size_t N, class T> auto ListFindHelper(const Indexed<N, T> &) -> Indexed<N, T>;
-template<size_t N, class List> struct ListFind;
-template<size_t N, template<class...>class L, class...Ts> struct ListFind<N, L<Ts...>>
-		: decltype(ListFindHelper<N>(typename MakeIndexFinder<Ts...>::type())) {};
+namespace Dalao {
+    
+    template<class T> struct type_identity { using type = T; };
+    template<class Seq> struct MakeIndexFinder;
+    template<size_t...I> struct MakeIndexFinder<std::index_sequence<I...>>
+    {
+        template<class T> type_identity<T> operator()(decltype(I, void()) *..., T *, ...);
+    };
+    template<size_t N, class List> struct ListFind;
+    template<size_t N, template<class...>class L, class...Ts> struct ListFind<N, L<Ts...>>
+    : decltype( MakeIndexFinder<std::make_index_sequence<N>>()(typename type_identity<Ts *>::type()...) ) {};
+    
+}
 
-// Naive one
-template<size_t N, class List> struct ListFind2;
-template<size_t N, template<class...>class L, class First, class...Rest> struct ListFind2<N, L<First, Rest...>>
-		: ListFind2<N - 1, L<Rest...>> {};
-template<template<class...>class L, class First, class...Rest> struct ListFind2<0, L<First, Rest...>>
-{
-	using type = First;
-};
+namespace Modern {
+    
+    template<size_t I, class T> struct Indexed { using type = T; };
+    template<class...Ts> struct MakeIndexFinder
+    {
+        template<class Seq> struct impl;
+        template<size_t...I> struct impl<std::index_sequence<I...>> : Indexed<I, Ts>... {};
+        using type = impl<std::index_sequence_for<Ts...>>;
+    };
+    template<size_t N, class T> auto ListFindHelper(const Indexed<N, T> &) -> Indexed<N, T>;
+    template<size_t N, class List> struct ListFind;
+    template<size_t N, template<class...>class L, class...Ts> struct ListFind<N, L<Ts...>>
+    : decltype(ListFindHelper<N>(typename MakeIndexFinder<Ts...>::type())) {};
+    
+}
+
+namespace Naive {
+    
+    template<size_t N, class List> struct ListFind;
+    template<size_t N, template<class...>class L, class First, class...Rest> struct ListFind<N, L<First, Rest...>>
+    : ListFind<N - 1, L<Rest...>> {};
+    template<template<class...>class L, class First, class...Rest> struct ListFind<0, L<First, Rest...>>
+    {
+        using type = First;
+    };
+    
+}
 
 // Make some test case
-template<class Seq, class T> struct MakeTypelist;
-template<size_t...I, class T> struct MakeTypelist<std::index_sequence<I...>, T>
+template<class Seq> struct MakeTypelist;
+template<size_t...I> struct MakeTypelist<std::index_sequence<I...>>
 {
-	using type = std::tuple<std::integral_constant<size_t, I*I>...>;
+    using type = std::tuple<std::integral_constant<size_t, I>...>;
 };
 int main()
 {
-	using A = MakeTypelist<std::make_index_sequence<65536>, char>::type;
-#if 1
-	using B = ListFind<255, A>::type;
+    using A = MakeTypelist<std::make_index_sequence<65535>>::type;
+    
+#define X 1
+    
+#if X == 1
+    using B = Dalao::ListFind<255, A>::type;
+#elif X == 2
+    using B = Modern::ListFind<255, A>::type;
 #else
-	using B = ListFind2<255, A>::type;
+    using B = Naive::ListFind<255, A>::type;
 #endif
-	std::cout << typeid(B).name() << std::endl;
-	return 0;
+    std::cout << typeid(B).name() << std::endl;
+    return 0;
 }
